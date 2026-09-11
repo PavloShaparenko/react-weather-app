68806a144d7ccda504f3180fbf84e7c342552add
Pavel_Shaparenko <151019903+PavloShaparenko@users.noreply.github.com>
2026-09-11 23:25:08 +0200

    Update App.js
    Removed comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react';
-// import bgGif from './shared/art/spring.webp'; // <--- TO USUŃ
+
 import './shared/styles/style.css';
 import Header from './widgets/header';
 import Footer from './widgets/Footer';
 import UpWeatherPc from './widgets/UpWeatherPc';
 import WeatherManager from './widgets/WeatherManager';
 import WeatherHighlights from './widgets/WeatherHighlights';
-import WeatherBackground from './widgets/WeatherBackground'; // <--- IMPORTUJ NOWY KOMPONENT
+import WeatherBackground from './widgets/WeatherBackground'; 
 import { fetchWeatherData } from './widgets/WeatherApi';
 
 function App() {
   const [searchedCity, setSearchedCity] = useState('Lublin');
   const [lang, setLang] = useState('en');
   const [weather, setWeather] = useState(null);
-  // const [loading, setLoading] = useState(true); // Opcjonalne, jeśli używasz loadera
+
 
   const handleCitySearch = (city) => {
     setSearchedCity(city);
@@ -36,14 +36,13 @@ function App() {
   return (
     <div className="app-container">
 
-      {/* --- NOWE DYNAMICZNE TŁO --- */}
-      {/* Przekazujemy pełne dane pogodowe do komponentu tła */}
+      
       <WeatherBackground weatherData={weather?.fullRawData} /> 
 
       <Header onSearch={handleCitySearch} lang={lang} changeLang={changeLanguage} />
       <UpWeatherPc Scity={searchedCity} lang={lang} />
       
-      {/* WeatherManager wciąż działa po swojemu */}
+      
       <WeatherManager city={searchedCity} lang={lang} />
       
       <WeatherHighlights data={weather?.fullRawData} lang={lang} />
